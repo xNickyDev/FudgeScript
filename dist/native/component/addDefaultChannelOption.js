4d4510554906c49ace0e787761c481435c4f768a
Nicky <111157596+xNickyDev@users.noreply.github.com>
2024-08-17 10:54:54 +0000

    Added console logs to debug

--- dist/native/component/addDefaultChannelOption.js
+++ dist/native/component/addDefaultChannelOption.js
@@ -5,6 +5,7 @@ const structures_1 = require("../../structures");
 exports.default = new structures_1.NativeFunction({
     name: "$addDefaultChannelOption",
     version: "1.4.0",
+    aliases: ["$addDefaultChannels"],
     description: "Adds a default channel option to the last select menu",
     unwrap: true,
     brackets: true,
@@ -19,8 +20,11 @@ exports.default = new structures_1.NativeFunction({
     ],
     execute(ctx, [channels]) {
         const menu = ctx.container.components.at(-1);
+        console.log("Type:", menu?.constructor.name);
+        console.log(menu);
         if (menu && menu instanceof discord_js_1.ChannelSelectMenuBuilder) {
             menu.addDefaultChannels(channels);
+            console.log("success");
         }
         return this.success();
     },
--- src/native/component/addDefaultChannelOption.ts
+++ src/native/component/addDefaultChannelOption.ts
@@ -5,6 +5,7 @@ import { MentionableSelectMenuBuilder, RoleSelectMenuBuilder } from "@discordjs/
 export default new NativeFunction({
     name: "$addDefaultChannelOption",
     version: "1.4.0",
+    aliases: ["$addDefaultChannels"],
     description: "Adds a default channel option to the last select menu",
     unwrap: true,
     brackets: true,
@@ -19,8 +20,13 @@ export default new NativeFunction({
     ],
     execute(ctx, [ channels ]) {
         const menu = ctx.container.components.at(-1)
+
+        console.log("Type:", menu?.constructor.name)
+        console.log(menu)
+
         if (menu && menu instanceof ChannelSelectMenuBuilder) {
             menu.addDefaultChannels(channels)
+            console.log("success")
         }
 
         return this.success()
