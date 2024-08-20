d0f1d7c1c5444b4cc0ff2e2af8b1060f39841b85
Nicky <111157596+xNickyDev@users.noreply.github.com>
2024-08-20 22:07:26 +0000

    Fixed 2 errors of prompt and ws

--- src/functions/prompt.ts
+++ src/functions/prompt.ts
@@ -2,7 +2,7 @@ import { stdin, stdout } from "process"
 import { createInterface } from "readline"
 
 export default async function(q: string) {
-    const itf = createInterface(stdin, stdout)
+    const itf = createInterface(stdin as unknown as NodeJS.ReadableStream, stdout as unknown as NodeJS.WritableStream)
     return new Promise<string>(r => {
         itf.question(q, input => {
             itf.close()
