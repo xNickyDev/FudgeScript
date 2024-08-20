415aec74e85984ad217cdbea35541edb1f6e6705
Nicky <111157596+xNickyDev@users.noreply.github.com>
2024-08-20 22:13:22 +0000

    Fixed NodeJS error

--- src/functions/prompt.ts
+++ src/functions/prompt.ts
@@ -2,7 +2,7 @@ import { stdin, stdout } from "process"
 import { createInterface } from "readline"
 
 export default async function(q: string) {
-    const itf = createInterface(stdin as unknown as NodeJS.ReadableStream, stdout as unknown as NodeJS.WritableStream)
+    const itf = createInterface(stdin as any, stdout as any)
     return new Promise<string>(r => {
         itf.question(q, input => {
             itf.close()
