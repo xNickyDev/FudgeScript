1a832c5c3d8a798fda035bdc47236c03e16fdc64
Nicky <111157596+xNickyDev@users.noreply.github.com>
2024-09-05 17:13:11 +0000

    Added $unescapeCode

---
+++ dist/native/other/unescapeCode.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const structures_1 = require("../../structures");
+exports.default = new structures_1.NativeFunction({
+    name: "$unescapeCode",
+    version: "1.5.0",
+    description: "Code inside this function will be executed",
+    unwrap: true,
+    brackets: true,
+    aliases: [
+        "$unescape",
+        "$nonEscape"
+    ],
+    args: [
+        {
+            name: "code",
+            type: structures_1.ArgType.String,
+            description: "The code to execute",
+            required: true,
+            rest: false
+        }
+    ],
+    output: structures_1.ArgType.String,
+    execute(ctx, [code]) {
+        const unescaped = code
+            .replace(/\\;/g, ";")
+            .replace(/\\]/g, "]")
+            .replace(/\\\$/g, "$")
+            .replace(/\\\\/g, "\\");
+        return this.success(unescaped);
+    },
+});
+//# sourceMappingURL=unescapeCode.js.map
---
+++ src/native/other/unescapeCode.ts
@@ -0,0 +1,32 @@
+import { ArgType, NativeFunction } from "../../structures"
+
+export default new NativeFunction({
+    name: "$unescapeCode",
+    version: "1.5.0",
+    description: "Code inside this function will be executed",
+    unwrap: true,
+    brackets: true,
+    aliases: [
+        "$unescape",
+        "$nonEscape"
+    ],
+    args: [
+        {
+            name: "code",
+            type: ArgType.String,
+            description: "The code to execute",
+            required: true,
+            rest: false
+        }
+    ],
+    output: ArgType.String,
+    execute(ctx, [code]) {
+        const unescaped = code
+            .replace(/\\;/g, ";")
+            .replace(/\\]/g, "]")
+            .replace(/\\\$/g, "$")
+            .replace(/\\\\/g, "\\")
+
+        return this.success(unescaped)
+    },
+})
