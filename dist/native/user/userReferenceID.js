a0fe5f4c3a7df6a13cf111f42174fe6312f06d24
Nicky <111157596+xNickyDev@users.noreply.github.com>
2024-09-07 12:22:06 +0000

    Added $userReferenceID

---
+++ dist/native/user/userReferenceID.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const structures_1 = require("../../structures");
+exports.default = new structures_1.NativeFunction({
+    name: "$userReferenceID",
+    version: "1.5.0",
+    description: "Returns the id of the user this message replies to",
+    unwrap: true,
+    output: structures_1.ArgType.User,
+    args: [
+        {
+            name: "channel ID",
+            rest: false,
+            required: true,
+            description: "The channel to get the message from",
+            type: structures_1.ArgType.Channel,
+            check: (i) => i.isTextBased()
+        },
+        {
+            name: "message ID",
+            description: "The message to get its reference user",
+            rest: false,
+            type: structures_1.ArgType.Message,
+            pointer: 0,
+            required: true,
+        },
+    ],
+    brackets: false,
+    async execute(ctx, [channel, message]) {
+        const msg = (message ?? ctx.message).reference?.messageId;
+        return this.success((await (channel ?? ctx.channel).messages.fetch(msg)).author.id);
+    }
+});
+//# sourceMappingURL=userReferenceID.js.map
