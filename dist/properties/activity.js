3876514d2d7fa545d0dff245a07d11754fc86885
xNickyDev <xnickydev@gmail.com>
2024-09-21 19:57:58 +0200

    Fix attempt

--- dist/properties/activity.js
+++ dist/properties/activity.js
@@ -38,9 +38,9 @@ exports.ActivityProperties = (0, defineProperties_1.default)({
     partySize: (i) => i?.party?.size[0],
     syncId: (i) => i?.syncId,
     url: (i) => i?.url,
-    largeText: (i) => i?.assets?.largeText,
-    largeImage: (i) => i?.assets?.largeImageURL(),
-    smallText: (i) => i?.assets?.smallText,
-    smallImage: (i) => i?.assets?.smallImageURL(),
+    largeText: (i) => i?.assets?.largeText || null,
+    largeImage: (i) => i?.assets?.largeImageURL() || null,
+    smallText: (i) => i?.assets?.smallText || null,
+    smallImage: (i) => i?.assets?.smallImageURL() || null,
 });
 //# sourceMappingURL=activity.js.map
--- src/properties/activity.ts
+++ src/properties/activity.ts
@@ -33,8 +33,8 @@ export const ActivityProperties = defineProperties<typeof ActivityProperty, Acti
     partySize: (i) => i?.party?.size[0],
     syncId: (i) => i?.syncId,
     url: (i) => i?.url,
-    largeText: (i) => i?.assets?.largeText,
-    largeImage: (i) => i?.assets?.largeImageURL(),
-    smallText: (i) => i?.assets?.smallText,
-    smallImage: (i) => i?.assets?.smallImageURL(),
+    largeText: (i) => i?.assets?.largeText || null,
+    largeImage: (i) => i?.assets?.largeImageURL() || null,
+    smallText: (i) => i?.assets?.smallText || null,
+    smallImage: (i) => i?.assets?.smallImageURL() || null,
 })
