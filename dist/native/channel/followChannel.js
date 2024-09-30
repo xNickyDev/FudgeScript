2aea049f0a832b28efbfabdb560eb11a42bf7507
xNickyDev <xnickydev@gmail.com>
2024-09-30 19:36:52 +0200

    Fixed previously added functions

--- dist/native/channel/followChannel.js
+++ dist/native/channel/followChannel.js
@@ -4,6 +4,7 @@ const discord_js_1 = require("discord.js");
 const structures_1 = require("../../structures");
 exports.default = new structures_1.NativeFunction({
     name: "$followChannel",
+    version: "1.5.0",
     description: "Follows given announcement channel, returns bool",
     brackets: true,
     unwrap: true,
--- dist/native/interaction/applicationSubCommandGroupName.js
+++ dist/native/interaction/applicationSubCommandGroupName.js
@@ -3,11 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const structures_1 = require("../../structures");
 exports.default = new structures_1.NativeFunction({
     name: "$applicationSubCommandGroupName",
+    version: "1.5.0",
     description: "Returns an application sub command group name",
     unwrap: false,
     output: structures_1.ArgType.String,
     execute(ctx) {
-        return this.success(ctx.interaction?.isChatInputCommand() ? ctx.interaction.options.getSubcommandGroup(false) : undefined);
+        return this.success(ctx.interaction && "options" in ctx.interaction && "getSubcommandGroup" in ctx.interaction.options ? ctx.interaction.options.getSubcommandGroup(false) : undefined);
     },
 });
 //# sourceMappingURL=applicationSubCommandGroupName.js.map
