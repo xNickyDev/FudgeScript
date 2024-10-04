f7dbdea3fd76a5e1059659a4d67899705799226a
xNickyDev <xnickydev@gmail.com>
2024-10-05 00:28:29 +0200

    Added more emoji functions

---
+++ dist/native/emoji/editEmoji.d.ts
@@ -0,0 +1,35 @@
+import { ArgType, NativeFunction } from "../../structures";
+declare const _default: NativeFunction<[{
+    name: string;
+    description: string;
+    rest: false;
+    required: true;
+    type: ArgType.Guild;
+}, {
+    name: string;
+    description: string;
+    rest: false;
+    required: true;
+    type: ArgType.GuildEmoji;
+}, {
+    name: string;
+    description: string;
+    rest: false;
+    required: true;
+    type: ArgType.String;
+}, {
+    name: string;
+    description: string;
+    rest: false;
+    required: false;
+    type: ArgType.String;
+}, {
+    name: string;
+    description: string;
+    rest: true;
+    required: false;
+    type: ArgType.Role;
+    pointer: number;
+}], true>;
+export default _default;
+//# sourceMappingURL=editEmoji.d.ts.map
