686eccc74debb1b0e1cdde6546804b9de327aa31
xNickyDev <xnickydev@gmail.com>
2024-11-07 20:52:19 +0100

    Fix attempt for $bar again

--- dist/functions/generateBar.js
+++ dist/functions/generateBar.js
@@ -4,18 +4,12 @@ exports.generateAdvancedBar = exports.generateBar = void 0;
 function generateBar(current, max, len = 10, fill = "█", empty = "▒", round = true, fillStart = "", fillEnd = "", emptyStart = "", emptyEnd = "") {
     let fillN = Math[round ? "round" : "trunc"](Math.min(current, max) / max * len);
     let emptyN = len - fillN;
-    let start = "";
-    let end = "";
-    if (fillN > 0) {
-        start = fillStart || "";
-        end = fillEnd || "";
-        fillN -= Math.max((fillStart ? 1 : 0) + (fillEnd ? 1 : 0), 0);
-    }
-    else if (emptyN > 0) {
-        start = emptyStart || "";
-        end = emptyEnd || "";
-        emptyN -= Math.max((emptyStart ? 1 : 0) + (emptyEnd ? 1 : 0), 0);
-    }
+    const start = fillN > 0 ? fillStart || "" : emptyStart || "";
+    const end = emptyN > 0 ? emptyEnd || "" : fillEnd || "";
+    if (fillN > 0)
+        fillN = Math.max(fillN - (fillStart ? 1 : 0) - (fillEnd ? 1 : 0), 0);
+    if (emptyN > 0)
+        emptyN = Math.max(emptyN - (emptyStart ? 1 : 0) - (emptyEnd ? 1 : 0), 0);
     return start + fill.repeat(fillN) + empty.repeat(emptyN) + end;
 }
 exports.generateBar = generateBar;
--- src/functions/generateBar.ts
+++ src/functions/generateBar.ts
@@ -1,18 +1,12 @@
 export function generateBar(current: number, max: number, len: number = 10, fill: string = "█", empty: string = "▒", round = true, fillStart: string = "", fillEnd: string = "", emptyStart: string = "", emptyEnd: string = "") {
     let fillN = Math[round ? "round" : "trunc"](Math.min(current, max) / max * len)
     let emptyN = len - fillN
-    let start = ""
-    let end = ""
+    
+    const start = fillN > 0 ? fillStart || "" : emptyStart || ""
+    const end = emptyN > 0 ? emptyEnd || "" : fillEnd || ""
 
-    if (fillN > 0) {
-        start = fillStart || ""
-        end = fillEnd || ""
-        fillN -= Math.max((fillStart ? 1 : 0) + (fillEnd ? 1 : 0), 0)
-    } else if (emptyN > 0) {
-        start = emptyStart || ""
-        end = emptyEnd || ""
-        emptyN -= Math.max((emptyStart ? 1 : 0) + (emptyEnd ? 1 : 0), 0)
-    }
+    if (fillN > 0) fillN = Math.max(fillN - (fillStart ? 1 : 0) - (fillEnd ? 1 : 0), 0)
+    if (emptyN > 0) emptyN = Math.max(emptyN - (emptyStart ? 1 : 0) - (emptyEnd ? 1 : 0), 0)
 
     return start + fill.repeat(fillN) + empty.repeat(emptyN) + end
 }
