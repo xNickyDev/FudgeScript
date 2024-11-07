e91b9106a8157a6cd1b0ccca38f024911675d267
xNickyDev <xnickydev@gmail.com>
2024-11-07 20:45:29 +0100

    Fixed $bar

--- src/functions/generateBar.ts
+++ src/functions/generateBar.ts
@@ -1,17 +1,20 @@
 export function generateBar(current: number, max: number, len: number = 10, fill: string = "█", empty: string = "▒", round = true, fillStart: string = "", fillEnd: string = "", emptyStart: string = "", emptyEnd: string = "") {
     let fillN = Math[round ? "round" : "trunc"](Math.min(current, max) / max * len)
     let emptyN = len - fillN
+    let start = ""
+    let end = ""
 
-    if (fillN > 0) fillN -= (fillStart ? 1 : 0) + (fillEnd ? 1 : 0)
-    if (emptyN > 0) emptyN -= (emptyStart ? 1 : 0) + (emptyEnd ? 1 : 0)
+    if (fillN > 0) {
+        start = fillStart || ""
+        end = fillEnd || ""
+        fillN -= Math.max((fillStart ? 1 : 0) + (fillEnd ? 1 : 0), 0)
+    } else if (emptyN > 0) {
+        start = emptyStart || ""
+        end = emptyEnd || ""
+        emptyN -= Math.max((emptyStart ? 1 : 0) + (emptyEnd ? 1 : 0), 0)
+    }
 
-    const start = fillN > 0 ? fillStart : emptyStart
-    const end = emptyN > 0 ? emptyEnd : fillEnd
-    
-    const filledBar = fill.repeat(fillN)
-    const emptyBar = empty.repeat(emptyN)
-
-    return (start || "") + filledBar + emptyBar + (end || "")
+    return start + fill.repeat(fillN) + empty.repeat(emptyN) + end
 }
 
 export function generateAdvancedBar(
