a60a4d244c6ee90e8a042ed69bdd601cb1479b23
xNickyDev <xnickydev@gmail.com>
2024-11-08 17:57:45 +0100

    Fixed $bar length

--- dist/functions/generateBar.js
+++ dist/functions/generateBar.js
@@ -5,21 +5,23 @@ function generateBar(current, max, len = 10, fill = "█", empty = "▒", round 
     let fillN = Math[round ? "round" : "trunc"](Math.min(Math.max(current, 0), max) / max * len);
     let emptyN = len - fillN;
     let start = "", end = "";
-    if (fillN > 0 && emptyN > 0) {
-        start = fillStart || "";
-        end = emptyEnd || "";
-        fillN -= start ? 1 : 0;
-        emptyN -= end ? 1 : 0;
-    }
-    else if (fillN > 0) {
-        start = fillStart || "";
-        end = fillEnd || "";
-        fillN -= (start ? 1 : 0) + (end ? 1 : 0);
-    }
-    else if (emptyN > 0) {
-        start = emptyStart || "";
-        end = emptyEnd || "";
-        emptyN -= (start ? 1 : 0) + (end ? 1 : 0);
+    if (len > 1) {
+        if (fillN > 0 && emptyN > 0) {
+            start = fillStart || "";
+            end = emptyEnd || "";
+            fillN -= start ? 1 : 0;
+            emptyN -= end ? 1 : 0;
+        }
+        else if (fillN > 0) {
+            start = fillStart || "";
+            end = fillEnd || "";
+            fillN -= (start ? 1 : 0) + (end ? 1 : 0);
+        }
+        else if (emptyN > 0) {
+            start = emptyStart || "";
+            end = emptyEnd || "";
+            emptyN -= (start ? 1 : 0) + (end ? 1 : 0);
+        }
     }
     fillN = Math.max(fillN, 0);
     emptyN = Math.max(emptyN, 0);
--- src/functions/generateBar.ts
+++ src/functions/generateBar.ts
@@ -1,21 +1,34 @@
-export function generateBar(current: number, max: number, len: number = 10, fill: string = "█", empty: string = "▒", round = true, fillStart: string = "", fillEnd: string = "", emptyStart: string = "", emptyEnd: string = "") {
+export function generateBar(
+    current: number,
+    max: number,
+    len: number = 10,
+    fill: string = "█",
+    empty: string = "▒",
+    round = true,
+    fillStart: string = "",
+    fillEnd: string = "",
+    emptyStart: string = "",
+    emptyEnd: string = ""
+): string {
     let fillN = Math[round ? "round" : "trunc"](Math.min(Math.max(current, 0), max) / max * len)
     let emptyN = len - fillN
     let start = "", end = ""
 
-    if (fillN > 0 && emptyN > 0) {
-        start = fillStart || ""
-        end = emptyEnd || ""
-        fillN -= start ? 1 : 0
-        emptyN -= end ? 1 : 0
-    } else if (fillN > 0) {
-        start = fillStart || ""
-        end = fillEnd || ""
-        fillN -= (start ? 1 : 0) + (end ? 1 : 0)
-    } else if (emptyN > 0) {
-        start = emptyStart || ""
-        end = emptyEnd || ""
-        emptyN -= (start ? 1 : 0) + (end ? 1 : 0)
+    if (len > 1) {
+        if (fillN > 0 && emptyN > 0) {
+            start = fillStart || ""
+            end = emptyEnd || ""
+            fillN -= start ? 1 : 0
+            emptyN -= end ? 1 : 0
+        } else if (fillN > 0) {
+            start = fillStart || ""
+            end = fillEnd || ""
+            fillN -= (start ? 1 : 0) + (end ? 1 : 0)
+        } else if (emptyN > 0) {
+            start = emptyStart || ""
+            end = emptyEnd || ""
+            emptyN -= (start ? 1 : 0) + (end ? 1 : 0)
+        }
     }
 
     fillN = Math.max(fillN, 0)
