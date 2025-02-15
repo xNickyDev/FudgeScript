c06225c49f9bd64699de51457474732570f732d6
xNickyDev <xnickydev@gmail.com>
2025-02-15 16:42:30 +0100

    test

--- src/native/other/unescapeCode.ts
+++ src/native/other/unescapeCode.ts
@@ -21,6 +21,6 @@ export default new NativeFunction({
     ],
     output: ArgType.String,
     execute(ctx) {
-        return this.unsafeSuccess(this.displayFields()?.join(";"))
+        return this.return(this.displayFields()?.join(";") || "")
     },
 })
