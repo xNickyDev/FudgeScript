a85315c63370f1662e872006330bb354d5c93334
xNickyDev <xnickydev@gmail.com>
2025-05-23 15:08:54 +0200

    small change

--- dist/native/message/attachment.js
+++ dist/native/message/attachment.js
@@ -54,8 +54,9 @@ exports.default = new structures_1.NativeFunction({
     execute(ctx, [url, name, asText, enc, desc, spoiler]) {
         const attachment = new discord_js_1.AttachmentBuilder(asText ? Buffer.from(url, enc ?? "utf-8") : url, {
             name,
-            description: desc || undefined
         }).setSpoiler(!!spoiler);
+        if (desc)
+            attachment.setDescription(desc);
         ctx.container.files.push(attachment);
         return this.success();
     },
