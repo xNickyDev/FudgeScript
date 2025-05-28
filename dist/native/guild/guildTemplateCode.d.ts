c4ae086e90fd518afd5675cdca30b1858c5370cd
xNickyDev <xnickydev@gmail.com>
2025-05-28 20:25:10 +0200

    Remade function

--- dist/native/guild/guildTemplateCode.d.ts
+++ dist/native/guild/guildTemplateCode.d.ts
@@ -1,17 +1,10 @@
 import { ArgType, NativeFunction } from "../../structures";
-import { TemplateProperty } from "./getGuildTemplate";
 declare const _default: NativeFunction<[{
     name: string;
     description: string;
     rest: false;
     required: true;
     type: ArgType.Guild;
-}, {
-    name: string;
-    description: string;
-    rest: false;
-    type: ArgType.Enum;
-    enum: typeof TemplateProperty;
 }], true>;
 export default _default;
-//# sourceMappingURL=guildTemplate.d.ts.map
+//# sourceMappingURL=guildTemplateCode.d.ts.map
