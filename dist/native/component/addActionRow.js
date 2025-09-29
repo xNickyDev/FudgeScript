286aa43e20e0e0ac70834e7c2519a570db2d1495
xNickyDev <xnickydev@gmail.com>
2025-09-30 00:12:28 +0200

    Test

--- dist/native/component/addActionRow.js
+++ dist/native/component/addActionRow.js
@@ -2,20 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 const structures_1 = require("../../structures");
+const components_1 = require("../../functions/components");
 exports.default = new structures_1.NativeFunction({
     name: "$addActionRow",
     version: "1.0.0",
     description: "Adds an action row",
     unwrap: true,
     execute(ctx) {
-        const row = ctx.container.actionRow;
-        const comp = ctx.container.components.at(-1);
-        if (row) {
-            if (comp instanceof discord_js_1.ContainerBuilder && ctx.container.isInside(discord_js_1.ComponentType.Container))
-                comp.addActionRowComponents(row);
-            else
-                ctx.container.components.push(row);
-        }
+        (0, components_1.addActionRow)(ctx, false);
         ctx.container.actionRow = new discord_js_1.ActionRowBuilder();
         return this.success();
     },
--- src/native/component/addActionRow.ts
+++ src/native/component/addActionRow.ts
@@ -1,5 +1,6 @@
-import { ActionRowBuilder, ComponentType, ContainerBuilder } from "discord.js"
+import { ActionRowBuilder } from "discord.js"
 import { NativeFunction, Return } from "../../structures"
+import { addActionRow } from "../../functions/components"
 
 export default new NativeFunction({
     name: "$addActionRow",
@@ -7,15 +8,7 @@ export default new NativeFunction({
     description: "Adds an action row",
     unwrap: true,
     execute(ctx) {
-        const row = ctx.container.actionRow
-        const comp = ctx.container.components.at(-1)
-
-        if (row) {
-            if (comp instanceof ContainerBuilder && ctx.container.isInside(ComponentType.Container))
-                comp.addActionRowComponents(row)
-            else ctx.container.components.push(row)
-        }
-
+        addActionRow(ctx, false)
         ctx.container.actionRow = new ActionRowBuilder()
         return this.success()
     },
