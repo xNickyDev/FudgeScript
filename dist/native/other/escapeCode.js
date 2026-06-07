3593c2c770f0e02a5fda4c16fcb0819a9318e906
xNickyDev <xnickydev@gmail.com>
2026-06-07 02:15:02 +0200

    New test

--- dist/native/other/escapeCode.js
+++ dist/native/other/escapeCode.js
@@ -22,7 +22,7 @@ exports.default = new structures_1.NativeFunction({
     output: structures_1.ArgType.String,
     execute(ctx) {
         const code = this.data.fields[0];
-        return this.success(code.value);
+        return this.success(code.functions.map((x) => x.display));
     },
 });
 //# sourceMappingURL=escapeCode.js.map
