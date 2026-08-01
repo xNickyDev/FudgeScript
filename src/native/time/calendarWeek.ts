f72e99ffddc81ac9af944ebbbec0c49e63a4e5ea
xNickyDev <xnickydev@gmail.com>
2026-08-01 03:56:18 +0200

    Test fix for $calendarWeek

--- src/native/time/calendarWeek.ts
+++ src/native/time/calendarWeek.ts
@@ -1,10 +1,4 @@
-import { ArgType, NativeFunction, Return } from "../../structures"
-
-function getWeekOfYear(date: Date) {
-    const start = new Date(date.getFullYear(), 0, 1)
-    const days = (date.getTime() - start.getTime()) / 86400000
-    return Math.ceil((days + start.getDay() + 1) / 7)
-}
+import { ArgType, CalendarType, NativeFunction } from "../../structures"
 
 export default new NativeFunction({
     name: "$calendarWeek",
@@ -12,7 +6,25 @@ export default new NativeFunction({
     description: "Returns the calendar week",
     unwrap: true,
     output: ArgType.Number,
-    execute: async function(ctx) {
-        return this.success(getWeekOfYear(new Date(new Date().toLocaleString("en-US", { timeZone: ctx.timezone, calendar: ctx.calendar }))))
+    execute: async function (ctx) {
+        const date = new Date(new Date().toLocaleString("en-US", { timeZone: ctx.timezone, calendar: ctx.calendar }))
+
+        if (ctx.calendar === CalendarType.Iso8601) {
+            const target = new Date(date.valueOf())
+            const dayNr = (date.getDay() + 6) % 7
+            target.setDate(target.getDate() - dayNr + 3)
+            const firstThursday = target.valueOf()
+            target.setMonth(0, 1)
+            if (target.getDay() !== 4) {
+                target.setMonth(0, 1 + ((4 - target.getDay() + 7) % 7))
+            }
+            return this.success(1 + Math.ceil((firstThursday - target.valueOf()) / 604800000))
+        }
+
+        const start = new Date(date.getFullYear(), 0, 1)
+        const days = (date.getTime() - start.getTime()) / 86400000
+        const week = Math.ceil((days + start.getDay() + 1) / 7)
+
+        return this.success(week)
     }
 })
